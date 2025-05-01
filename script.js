11f2bf6805c54631d82bfe74c1c281c6512fd49d
priyanshi276 <priynshiigupta@gmail.com>
2025-05-01 20:50:48 +0530

    new

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // DOM elements selection
 let shoppingCart = document.querySelector('.shopping-cart');
+
 let searchForm = document.querySelector('.search-form');
 let loginForm = document.querySelector('.login-form');
 let navbar = document.querySelector('.navbar');
