a251afa4e312054a5b4a2d025c4182dc74963077
priyanshi276 <priynshiigupta@gmail.com>
2025-05-01 20:51:30 +0530

    line

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 let shoppingCart = document.querySelector('.shopping-cart');
 
 let searchForm = document.querySelector('.search-form');
+
 let loginForm = document.querySelector('.login-form');
 let navbar = document.querySelector('.navbar');
 
